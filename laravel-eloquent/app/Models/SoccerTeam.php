6f3398ee2e2c7e5ed941d84f53db61cd05eb47d2
Ligiano Azzi <ldaazzi@gmail.com>
2017-12-06 16:39:21 -0200

    class seven - Criando relacionamento one-to-many

--- laravel-eloquent/app/Models/SoccerTeam.php
+++ laravel-eloquent/app/Models/SoccerTeam.php
@@ -9,4 +9,12 @@ class SoccerTeam extends Model
     protected $fillable = [
     	'name'
     ];
+
+
+    //one-to-many
+    // o nome no plural ja indica que o relacionamento é 1 x N
+    public function clients(){
+    	return $this->hasMany( Client::class );
+    }
+
 }
